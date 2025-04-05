8679d8bf44c0749a91c4079e16d1c38ac976df65
Muhammad Hassaan Moazan <boxloid0321321@gmail.com>
2025-04-06 03:53:55 +0500

    Initial commit

---
+++ HireFireBackend/src/main/java/com/HireFire/HireFireBackend/HireFireBackendApplication.java
@@ -0,0 +1,14 @@
+package com.HireFire.HireFireBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//main entry point
+@SpringBootApplication
+public class HireFireBackendApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(HireFireBackendApplication.class, args);
+	}
+}
