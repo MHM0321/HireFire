cf21ae519e5760859cf1a47a830e553c7ec650e8
eishal-fatima-qadri <eishalfatima5723@gmail.com>
2025-04-30 23:51:20 +0500

    added header component and integrated main review, feedback and report pages

--- HireFireFrontend/app/_layout.tsx
+++ HireFireFrontend/app/_layout.tsx
@@ -1,17 +1,21 @@
 // layout.tsx
-import { Stack } from 'expo-router';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useColorScheme } from 'react-native';
+import { Stack } from "expo-router";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useColorScheme } from "react-native";
 
 const CustomDarkTheme = {
   ...DarkTheme,
   colors: {
     ...DarkTheme.colors,
-    background: '#1A0D0E',
-    card: '#1A0D0E',
-    text: '#FFFFFF',
-    border: '#333333',
-    primary: '#FF4D4D',
+    background: "#1A0D0E",
+    card: "#1A0D0E",
+    text: "#FFFFFF",
+    border: "#333333",
+    primary: "#FF4D4D",
   },
 };
 
@@ -19,75 +23,47 @@ const CustomLightTheme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    background: '#FFFFFF',
-    card: '#FFFFFF',
-    text: '#000000',
-    border: '#CCCCCC',
-    primary: '#FF4D4D',
+    background: "#FFFFFF",
+    card: "#FFFFFF",
+    text: "#000000",
+    border: "#CCCCCC",
+    primary: "#FF4D4D",
   },
 };
 
-
 export default function Layout() {
-  const colorScheme = 'dark';
-  
+  const colorScheme = "dark";
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? CustomDarkTheme : CustomLightTheme}>
+    <ThemeProvider
+      value={colorScheme === "dark" ? CustomDarkTheme : CustomLightTheme}
+    >
       <Stack>
-        <Stack.Screen 
-          name="index" 
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen 
-          name="customerPg"
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen 
-          name="redButton" 
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen 
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="customerPg" options={{ headerShown: false }} />
+        <Stack.Screen name="redButton" options={{ headerShown: false }} />
+        <Stack.Screen
           name="customerPg2"
           options={{
-          headerTitle: '', }}
+            headerTitle: "",
+          }}
         />
-        <Stack.Screen 
+        <Stack.Screen
           name="customerPg3"
           options={{
-          headerTitle: '', }}
+            headerTitle: "",
+          }}
         />
-        <Stack.Screen 
-          name="customerPg4"
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen 
-          name="appointmentsPg"
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen 
-          name="chatPg"
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen 
-          name="earningsPg"
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen 
-          name="myClientsPg"
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen 
-          name="myPerformancePg"
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen 
-          name="rateAndReviewPg"
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen 
-          name="workerPg"
-          options={{ headerShown: false }} 
-        />
+        <Stack.Screen name="customerPg4" options={{ headerShown: false }} />
+        <Stack.Screen name="appointmentsPg" options={{ headerShown: false }} />
+        <Stack.Screen name="chatPg" options={{ headerShown: false }} />
+        <Stack.Screen name="earningsPg" options={{ headerShown: false }} />
+        <Stack.Screen name="myClientsPg" options={{ headerShown: false }} />
+        <Stack.Screen name="myPerformancePg" options={{ headerShown: false }} />
+        <Stack.Screen name="rateAndReviewPg" options={{ headerShown: false }} />
+        <Stack.Screen name="workerPg" options={{ headerShown: false }} />
+        <Stack.Screen name="ReviewPg" options={{ headerShown: true }} />
+        <Stack.Screen name="FeedbackPage" options={{ headerShown: true }} />
       </Stack>
     </ThemeProvider>
   );
